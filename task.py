568d2e897eb4e0633de0746ddb48f18024363431
VictorVladlenovch <121546893+VictorVladlenovch@users.noreply.github.com>
2023-01-26 13:10:39 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,26 @@
+class Cat:
+    def __init__(self, name ='sam', gender = 'boy', age =2 ):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+cats = [
+    {
+    'name': 'Сэм',
+    'gender': 'мальчик',
+    'age': 2
+    },
+    {'name': 'Барон',
+    'gender': 'мальчик',
+    'age': 2
+    },
+    {'name': 'Барон',
+     'gender': 'мальчик',
+     'age': 2
+     },
+]
+#
+# def Hello_my():
+#     print('Запуск функции из импортируемого модуля') #если импорт всего модуля
+
+
